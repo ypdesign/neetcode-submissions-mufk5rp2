609970dec13bf5623342db740d5645e791581f73
Yug Divyang Patel <yugpatel196@gmail.com>
2026-04-05 12:18:30 +0530

    Add: foreign-dictionary - submission-2

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-2.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string foreignDictionary(vector<string>& words) {
+      int n=words.size();
+      vector<vector<bool>>adj(26,vector<bool>(26,false));
+      vector<int>indegree(26);
+      vector<bool>vis(26);
+      string ans;
+      for(auto s: words){
+        for(auto c : s){
+            vis[c-'a']=true;
+        }
+      }
+      for(int i=0;i<n-1;i++){
+          string w1=words[i];
+          string w2=words[i+1];
+          bool found = false;
+          for(int j=0;j<min(w1.size(),w2.size());j++){
+              if(w1[j] != w2[j]){
+                if(!adj[w1[j]-'a'][w2[j]-'a']){
+                    adj[w1[j]-'a'][w2[j]-'a']=true;
+                    indegree[w2[j]-'a']++;
+                }
+                found = true;
+                break;
+              }
+          }
+          if(!found && w1.size() > w2.size())
+                return "";
+      }
+      queue<int>q;
+      for(int i=0;i<26;i++){
+        if(indegree[i]== 0 && vis[i]){
+            q.push(i);
+        }
+      }
+      while(!q.empty()){
+         int node=q.front();
+         q.pop();
+         ans+=(char)(node+'a');
+         for(int i=0;i<26;i++){
+         if(adj[node][i]){
+            indegree[i]--;
+            if(indegree[i] == 0 && vis[i])
+                q.push(i);
+         }
+         }
+      }
+      int total=0;
+      for(int i=0;i<26;i++){
+        if(vis[i])total++;
+      }
+      if(ans.size() != total) return "";
+      return ans;
+    }
+};
